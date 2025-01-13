c29007d8c1fb13bcc00582cd72e22202927aa38b
Rodion Kholodov <rodionn39@gmail.com>
2025-01-13 14:45:37 +0300

    add toggle eye to text field

--- lib/features/login+registration/widgets/custom_text_field.dart
+++ lib/features/login+registration/widgets/custom_text_field.dart
@@ -1,28 +1,84 @@
+// import 'package:flutter/material.dart';
+
+// class CustomTextField extends StatelessWidget {
+//   final String hintText;
+//   final bool isPassword;
+//   final TextEditingController controller;
+
+//   const CustomTextField({
+//     super.key, 
+//     required this.hintText,
+//     this.isPassword = false,
+//     required this.controller,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextField(
+//       controller: controller,
+//       obscureText: isPassword,
+//       decoration: InputDecoration(
+//         hintText: hintText,
+//         border: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(8.0),
+//         ),
+//         contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final String hintText;
   final bool isPassword;
   final TextEditingController controller;
 
   const CustomTextField({
-    super.key, 
+    super.key,
     required this.hintText,
     this.isPassword = false,
     required this.controller,
   });
 
   @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late bool _isPasswordVisible;
+
+  @override
+  void initState() {
+    super.initState();
+    _isPasswordVisible = widget.isPassword;
+  }
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isPasswordVisible = !_isPasswordVisible;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: controller,
-      obscureText: isPassword,
+      controller: widget.controller,
+      obscureText: _isPasswordVisible,
       decoration: InputDecoration(
-        hintText: hintText,
+        hintText: widget.hintText,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8.0),
         ),
         contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  _isPasswordVisible ? Icons.visibility_off : Icons.visibility,
+                ),
+                onPressed: _togglePasswordVisibility,              
+              )
+            : null,
       ),
     );
   }
