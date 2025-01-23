b8ae81ab6e21d19fe71442b4ade3b9543fedbd29
Rodion Kholodov <rodionn39@gmail.com>
2025-01-23 10:52:47 +0300

    change login func

--- lib/features/login+registration/utils/auth_manager.dart
+++ lib/features/login+registration/utils/auth_manager.dart
@@ -18,15 +18,26 @@ class AuthManager {
   static void logout() {
     _storage.remove('isLoggedIn');
     _storage.remove('username');
+    _storage.remove('userId');
   }
 
-    // Сохранение имени пользователя
+  // Сохранение имени пользователя
   static Future<void> setUsername(String username) async {
     await _storage.write('username', username);
   }
 
-  // Получение имени пользователя
+  // Получение id пользователя
   static String? getUsername() {
     return _storage.read('username');
+  }
+
+  // Сохранение id пользователя
+  static Future<void> setUserId(int userId) async {
+    await _storage.write('userId', userId);
+  }
+
+  // Получение имени пользователя
+  static int? getUserId() {
+    return _storage.read('userId');
   }
 }
