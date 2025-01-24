a968268e82c2f52204594228fb3f518713506908
Rodion Kholodov <rodionn39@gmail.com>
2025-01-24 03:20:54 +0300

    fix db config to dev ver

--- lib/features/database_config.dart
+++ lib/features/database_config.dart
@@ -1,7 +1,7 @@
 class DatabaseConfig {
   static const String host = '79.137.204.140';
   static const int port = 5000;
-  static const String database = 'cognitive';
-  static const String username = 'cognitive_app';
-  static const String password = 'cognitive_app';
+  static const String database = 'cognitive_dev';
+  static const String username = 'cognitive_developer';
+  static const String password = 'cognitive_developer';
 }
