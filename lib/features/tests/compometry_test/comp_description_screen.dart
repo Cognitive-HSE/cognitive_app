70b73db66262f6133ea55886416211be158daf57
nicko587 <nickol.shishloff@gmail.com>
2025-02-03 11:25:59 +0300

    -

--- lib/features/tests/compometry_test/comp_description_screen.dart
+++ lib/features/tests/compometry_test/comp_description_screen.dart
@@ -27,6 +27,12 @@ class _CampimetryDescriptionScreenState extends State<CampimetryDescriptionScree
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text(
+          'Тест "Кампиметрия"',
+          style: TextStyle(color: Colors.white), // Белый цвет текста
+        ),
+        backgroundColor: Color(0xFF373737),
+        centerTitle: true,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
@@ -34,22 +40,33 @@ class _CampimetryDescriptionScreenState extends State<CampimetryDescriptionScree
           },
         ),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
+      body: Container(
+        color: Color(0xFF373737), // Фон страницы
+        child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(
-                width: 300,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
                 child: Text(
-                  'Описание теста: \nНа первом этапе нужно нажимать кнопку "Добавить оттенок" пока силуэт не станет полностью видимым. После этого нужно выбрать силуэт. На втором этапе нужно нажимать кнопку "Убавить оттенок", пока силуэт не станет полностью невидимым, и когда решите, что больше не видите, нажимайте "Не вижу".',
+                  'Описание теста: \n\nНа первом этапе нужно нажимать кнопку "Добавить оттенок" пока силуэт не станет полностью видимым. После этого нужно выбрать силуэт. На втором этапе нужно нажимать кнопку "Убавить оттенок", пока силуэт не станет полностью невидимым, и когда решите, что больше не видите, нажимайте "Не вижу".',
                   textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white, // Белый цвет текста
+                  ),
                 ),
               ),
               const SizedBox(height: 20),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[700], // Цвет кнопки
+                  foregroundColor: Colors.white, // Цвет текста кнопки
+                  minimumSize: const Size(200, 50), // Размер кнопки
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // Скругленные углы
+                  ),
+                ),
                 onPressed: _goToTest,
                 child: const Text('Начать тест'),
               ),
@@ -60,3 +77,4 @@ class _CampimetryDescriptionScreenState extends State<CampimetryDescriptionScree
     );
   }
 }
+
